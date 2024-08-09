850af79eab7da80556dd8a260e93363db149b950
18Abhinav07 <abhinavpangaria2003@gmail.com>
2024-08-09 08:00:38 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}).then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch((err) => console.log(err));
 
 // MongoDB Models
 const playerSchema = new mongoose.Schema({
